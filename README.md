b718e5c092cedbbe10b85a08754af777796d340e
tarneo <tarneo@tarneo.fr>
2025-08-30 22:54:26 +0200

    Allow building locally using Nix

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ This is the ZMK config for the [Triboard](https://github.com/tarneaux/triboard),
 It is heavily based on GEIGEIGEIST's [TOTEM ZMK config](https://github.com/GEIGEIGEIST/zmk-config-totem).
 
 ![Keymap drawing](https://github.com/user-attachments/assets/0a14ae99-9cc9-4066-b515-f84c9055994b)
+
+## Building with Nix
+
+```sh
+nix run .#flash
+```
+
+To mount the devices:
+```sh
+doas mount /dev/disk/by-label/XIAO-SENSE /mnt -o uid=1000 -o gid=100
+```
+
+## Maintenance
+
+Run `nix run .#update` to update West dependencies, including ZMK version, and bump the zephyrDepsHash on the derivation
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, zmk-nix }: let
+    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+  in {
+    packages = forAllSystems (system: rec {
+      default = firmware;
+
+      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+        name = "firmware";
+
+        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ".h" ];
+
+        board = "seeeduino_xiao_ble";
+        shield = "triboard_%PART%";
+
+        zephyrDepsHash = "sha256-79/rYCtUDlC0K4ARO9MSEaCcI1RQSsv7MCeayVZSwtQ=";
+
+        meta = {
+          description = "ZMK firmware";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
+        };
+      };
+
+      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+      update = zmk-nix.packages.${system}.update;
+    });
+
+    devShells = forAllSystems (system: {
+      default = zmk-nix.devShells.${system}.default;
+    });
+  };
+}
